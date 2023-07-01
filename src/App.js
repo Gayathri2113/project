beca666698caaada826e5f11e0d9ab488f81944d
Gayathri2113 <gayathribhukya211@gmail.com>
2023-07-01 23:16:51 +0530

    Added github and netlify links

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="App">
         <Weather />
         <footer>
-          <p>This code is coded by <a href="https://github.com/Gayathri2113/project">gayathri</a> and it is open-sourced</p>
+          <p>This code is coded by <a href="https://github.com/Gayathri2113/project" target="_blank" rel="noreferrer">gayathri</a> and it is hosted on <a href="https://64a0650a992e0e4de66f1ef4--comforting-sunburst-2444bb.netlify.app/" target="_blank" rel="noreferrer">netlify</a></p>
         </footer>
       </div>
     </div >
